cb670f2252272d17bb93bc214c7f12273ac0b7e0
Ali Shafiee <alshf89@gmail.com>
2020-06-24 18:30:10 +0430

    Setup laravel passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+use Laravel\Passport\RouteRegistrar;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,26 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        $this->registerPassport();
+    }
+
+    /**
+     * Setup Laravel Passport.
+     *
+     * @return void
+     */
+    protected function registerPassport()
+    {
+        Passport::routes(function (RouteRegistrar $router) {
+            // Handle Client Credential and Password Grants Routes
+            $router->forAccessTokens();
+
+            // Handle Refresh Tokens Routes
+            $router->forTransientTokens();
+        });
+
+        Passport::tokensExpireIn(now()->addweeks(4));
+        Passport::refreshTokensExpireIn(now()->addweeks(12));
     }
 }
