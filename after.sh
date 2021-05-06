b2e67960521d504caaf1cf9cba0612b664713f6e
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2021-05-06 23:57:00 +0430

    faster deploys

--- after.sh
+++ after.sh
@@ -2,7 +2,7 @@
 cd /sites/lairgg-services
 sudo rm -rf error_log
 sudo rm -rf access_log
-sudo composer install -n
+#sudo composer install -n
 sudo chmod -R 0777 storage bootstrap/cache
 sudo php artisan storage:link
 sudo chown nginx:nginx /sites -R
