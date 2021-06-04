dd39f3169df7723a7af5381b7c38192ac2d94a03
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2021-06-05 02:15:31 +0430

    team invitation

--- app/Repositories/InvitationRepository.php
+++ app/Repositories/InvitationRepository.php
@@ -150,7 +150,7 @@ class InvitationRepository extends BaseRepository
                     ];
                 } else {
                     $gameTitle = $invitedToObject->game->title;
-                    $tournamentDetails = [
+                    $teamDetails = [
                         'game' => $gameTitle,
                     ];
                 }
