6fc0d447ec3a4ffba52c94be344782b1b7999743
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2021-10-05 13:25:29 +0330

    single player tournament

---
+++ app/Jobs/TournamentPlayerCreator.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Enums\ParticipantAcceptanceState;
+use App\Participant;
+use App\Tournament;
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class TournamentPlayerCreator implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var Tournament $tournament
+     */
+    protected $tournament;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param int $tournamentId
+     */
+    public function __construct(int $tournamentId)
+    {
+        $this->tournament = Tournament::find($tournamentId);
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (! $this->tournament) {
+            return;
+        }
+        $this->tournament->engine()->createBracket();
+        $playersCount = $this->tournament->max_teams;
+        $users = User::query()->orderBy('id', 'desc')->limit($playersCount);
+        foreach ($users as $user) {
+            $participant = new Participant([
+                'participantable_type' => User::class,
+                'participantable_id' => $user->id,
+                'status' => ParticipantAcceptanceState::ACCEPTED,
+            ]);
+            $participant = $this->tournament->participants()->save($participant);
+            $this->tournament->engine()->assignParticipantToFirstEmptyMatch($participant);
+        }
+    }
+}
--- app/Participant.php
+++ app/Participant.php
@@ -14,6 +14,7 @@ class Participant extends Model
         'tournament_id',
         'participantable_type',
         'participantable_id',
+        'status',
     ];
 
     /**
