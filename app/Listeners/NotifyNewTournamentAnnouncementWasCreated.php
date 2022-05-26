0b6b0d5340a483948737f848e713b0cba3f3b9c2
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2022-05-26 13:45:48 +0430

    tournament announcement notification

--- app/Listeners/NotifyNewTournamentAnnouncementWasCreated.php
+++ app/Listeners/NotifyNewTournamentAnnouncementWasCreated.php
@@ -36,7 +36,7 @@ class NotifyNewTournamentAnnouncementWasCreated implements ShouldQueue
         $announcement = $event->announcement;
         $tournament = $announcement->tournament;
         $participants = $tournament->participants()->whereIn('status', [ParticipantAcceptanceState::ACCEPTED, ParticipantAcceptanceState::ACCEPTED_NOT_READY])->get();
-        $title = 'Tournament Bracket';
+        $title = 'Tournament Announcement';
         $body = __('notifications.tournament.announcement', [
             'tournament' => $tournament->title,
         ]);
